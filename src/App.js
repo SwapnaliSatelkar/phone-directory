559402e6b1faac9bf23b9ae89f23bbc4885637c8
unknown <swapnali@cdac.in>
2024-04-06 17:07:57 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@
 function App() {
   return (
     <div>
-      Phone Directory
+      {/* <div className="header">
+        Phone Directory
+      </div>
+      <button> Add </button>
+      <div>
+        <span> Name </span><br/>
+        <span> Phone </span>
+      </div> */}
+      <label htmlFor="name">Name: </label>
+      <input id="name" type="text" placeholder="Type here"/>
     </div>
   );
 }
