d4c5af804c394f7d73f2da78c17c0308fba021dc
unknown <swapnali@cdac.in>
2024-04-09 17:24:22 +0530

    Created  header components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 
 function App() {
-  return (
+
+    // let x=1;
+    // let y=2;
+  return (    
     <div>
-      {/* <div className="header">
+      <div className="header">
         Phone Directory
       </div>
       <button> Add </button>
       <div>
         <span> Name </span><br/>
         <span> Phone </span>
-      </div> */}
-      <label htmlFor="name">Name: </label>
-      <input id="name" type="text" placeholder="Type here"/>
-    </div>
-  );
-}
+      </div>
+      {/* <label htmlFor="name">Name: </label>
+      <input id="name" type="text" placeholder="Type here" defaultValue={x+y}/> */}
 
+
+    </div>
+
+    
+  );
+
+}
 export default App;
